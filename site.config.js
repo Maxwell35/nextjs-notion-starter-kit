cd048c75f689dc0aaf81645fe86f7cc2578ae6b0
Maxwell35 <max.zp.chen@gmail.com>
2022-01-31 11:19:32 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,23 +1,23 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '1fde0379bfc5457f82a6ac2b43756f03',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Zarathustra Daydream',
+  domain: 'https://stone-ceres-728.notion.site/mC-BloG-1fde0379bfc5457f82a6ac2b43756f03',
+  author: 'Max Chen',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Zarathustra Daydream',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
+  twitter: 'maxzpchen',
   github: 'transitive-bullshit',
   linkedin: 'fisch2',
 
